290bf6d0a79c84f5f7b13908a1d42acfe6bacc49
Sergey <kuzmenkos1986@gmail.com>
2016-01-11 23:18:36 +0200

    footer finish

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -1,5 +1,26 @@
 $(function(){
 
+    $('.btn-up').on({
+        'click':function(){
+            $('html, body').animate({scrollTop: 0}, 600);
+        }
+    });
+
+    $(window).on({
+        'scroll':function(){
+            if($(window).scrollTop() > $(window).height()){
+                $('.btn-up').fadeIn();
+            } else {
+                $('.btn-up').fadeOut();
+            }
+            if($(window).scrollTop() + $(window).height() > $('.site__footer').offset().top + 16) {
+                $('.btn-up').css({ bottom: $(window).scrollTop() + $(window).height() - $('.footer').offset().top + 16 });
+            } else {
+                $('.btn-up').css({ bottom: '16px' });
+            }
+        }
+    });
+
 } );
 
 var Shablon = function (obj) {
